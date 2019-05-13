8efbdfd80987964c40c91d86699b54c76f83b5fc
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-13 03:46:40 -0400

    :arrow_up_small: update to header organization
    Updated header organization to allow for easier styling and later responsiveness.

--- includes/_head.php
+++ includes/_head.php
@@ -25,7 +25,7 @@
       <span></span>
       <span></span>
     </button>
-    <div id="menu">
+    <div class="hidden" id="menu">
       <span id="search">
         <object id="search_icon" data="img/icons/search_icon.svg" type="image/svg+xml">
           <!-- put in png -->
@@ -34,52 +34,52 @@
           <input type="search" name="search" id="search">
         </form>
       </span>
-      <nav>
-        <div id="filters">
-          <button id="filter_trigger">
-            <p>Browse Recipes</p>
-          </button>
-          <div id="filter_drop">
-            <section class="filterType">
-              <h4>Main Ingredient</h4>
-              <ul>
-                <li><a href="#">Beef</a></li>
-                <li><a href="#">Chicken</a></li>
-                <li><a href="#">Pasta</a></li>
-                <li><a href="#">Pork</a></li>
-                <li><a href="#">Vegetables</a></li>
-              </ul>
-            </section>
-            <section class="filterType">
-              <h4>Cuisine</h4>
-              <ul>
-                <li><a href="#">American</a></li>
-                <li><a href="#">Asian</a></li>
-                <li><a href="#">Italian</a></li>
-              </ul>
-            </section>
-            <secion class="filterType">
-              <h4>Cook Time</h4>
-              <ul>
-                <li><a href="#">20 Minutes</a></li>
-                <li><a href="#">30 Minutes</a></li>
-                <li><a href="#">45 Minutes</a></li>
-                <li><a href="#">1 Hour</a></li>
-              </ul>
-            </secion>
-            <section class="filterType">
-              <h4>Servings</h4>
-              <ul>
-                <li><a href="#">1 Serving</a></li>
-                <li><a href="#">2 Servings</a></li>
-                <li><a href="#">4 Servings</a></li>
-              </ul>
-            </section>
-          </div>
+      <div id="filters"> 
+        <button id="filter_trigger" class="red-btn">
+          <p>Browse Recipes</p>
+        </button>
+        <div class="hidden" id="filter_drop">
+          <section class="filter_type">
+            <h4 class="filter_heading">Main Ingredient</h4>
+            <ul class="filter_list">
+              <li><a href="#">Beef</a></li>
+              <li><a href="#">Chicken</a></li>
+              <li><a href="#">Pasta</a></li>
+              <li><a href="#">Pork</a></li>
+              <li><a href="#">Vegetables</a></li>
+            </ul>
+          </section>
+          <section class="filter_type">
+            <h4 class="filter_heading">Cuisine</h4>
+            <ul class="filter_list">
+              <li><a href="#">American</a></li>
+              <li><a href="#">Asian</a></li>
+              <li><a href="#">Italian</a></li>
+            </ul>
+          </section>
+          <secion class="filter_type">
+            <h4 class="filter_heading">Cook Time</h4>
+            <ul class="filter_list">
+              <li><a href="#">20 Minutes</a></li>
+              <li><a href="#">30 Minutes</a></li>
+              <li><a href="#">45 Minutes</a></li>
+              <li><a href="#">1 Hour</a></li>
+            </ul>
+          </secion>
+          <section class="filter_type">
+            <h4 class="filter_heading">Servings</h4>
+            <ul class="filter_list">
+              <li><a href="#">1 Serving</a></li>
+              <li><a href="#">2 Servings</a></li>
+              <li><a href="#">4 Servings</a></li>
+            </ul>
+          </section>
         </div>
-        <a href="#" class="navLink">All Recipes</a>
-        <a href="about.php" class="navLink">About Us</a>
-        <a href="contact.php" class="navLink">Contact Us</a>
+      </div>
+      <nav>
+        <a href="#" class="nav_link red-btn">All Recipes</a>
+        <a href="about.php" class="nav_link red-btn">About Us</a>
+        <a href="contact.php" class="nav_link red-btn">Contact Us</a>
       </nav>
     </div>
   </header>
