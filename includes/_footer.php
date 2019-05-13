f73cb4b1695df97bbad7aaa113ca07ea5e438b32
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-13 03:42:07 -0400

    :heavy_plus_sign: add social media icons
    Added icons for fake social media accounts.
    
    To do:
       - Add links to social media homepages
       - Add hover/active states to icons

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,19 +1,29 @@
   <footer>
     <div id="social">
       <h6>Follow Us</h6>
-      <img src="img/social/facebook.svg" alt="Facebook Logo">
-      <img src="img/social/twitter.svg" alt="Twitter Logo">
-      <img src="img/social/instagram.svg" alt="Instagram Logo">
+      <object class="soc-icon" data="img/icons/social/facebook_1024.svg" type="image/svg+xml">
+        <!-- get png -->
+      </object>
+      <object class="soc-icon" data="img/icons/social/Twitter_Social_Icon_Circle_White.svg" type="image/svg+xml">
+        <!-- get png -->
+      </object>
+      <object class="soc-icon" data="img/icons/social/badgeRGB.svg" type="image/svg+xml">
+        <!-- get png -->
+      </object>
     </div>
-    <div id="footerNav">
+    <div id="footer_nav">
       <ul>
-        <li class="footLink"><a href="index.php">Home</a></li>
-        <li class="footLink"><a href="about.php">About Us</a></li>
-        <li class="footLink"><a href="contact.php">Contact Us</a></li>
-        <li class="footLink"><a href="support.php">Support</a></li>
+        <li class="foot_link"><a href="index.php">Home</a></li>
+        <li class="foot_link"><a href="about.php">About Us</a></li>
+        <li class="foot_link"><a href="contact.php">Contact Us</a></li>
+        <li class="foot_link"><a href="support.php">Support</a></li>
       </ul>
     </div>
   </footer>
+
+  <?php
+    include "includes/_alpha_links.php"
+  ?>
 
   <script src="js/main.js"></script>
 </body>
