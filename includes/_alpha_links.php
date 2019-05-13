c0f4c702cfba326ff0ab185923d814d0f475ae7a
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-13 03:45:09 -0400

    :one: initial commit of alpha links
    Created include doc that includes links to all alpha assignment links. Set up as an include doc to easily remove later.

---
+++ includes/_alpha_links.php
@@ -0,0 +1,10 @@
+<div id="alpha">
+  <h2>Alpha Links</h2>
+  <ul>
+    <li><a href="index.php">Home Screen</a></li>
+    <li><a href="recipeTemplate.php">Recipe Template</a></li>
+    <li><a href="search.php">Search/Filter Results Template</a></li>
+    <li><a href="noresults.php">No Results Page</a></li>
+    <li><a href="support.php">Help Page</a></li>
+  </ul>
+</div>
