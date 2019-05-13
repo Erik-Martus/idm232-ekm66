2dc2112144183224a6cea3b799c3b171d4e607a0
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-13 03:39:21 -0400

    :tada: complete index mobile styling.
    Completed mobile styling for index page. Styling set up to be easily applied to remaining pages to rapidly develop the rest of the project.

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
   <section id="hero">
     <a href="recipeTemplate.php"><img src="img/recipes/ancho-orange_chicken/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Ancho-Orange Chicken"></a>
     <h3>Ancho-Orange Chicken</h3>
-    <h5 class="subtitle">with Kale, Rice & Roasted Carrots</h5>
-    <a href="recipeTemplate.php">Get Cooking</a>
+    <h5 class="rec_subtitle">with Kale, Rice & Roasted Carrots</h5>
+    <a class="red-btn rec-btn" href="recipeTemplate.php">Get Cooking</a>
   </section>
 
   <!-- Mission Statement -->
@@ -19,25 +19,25 @@
     <p>Cooking isn't difficult, everyone can do it (yes, even you). Our recipes come with step-by-step instructions that guide you from raw ingredients through finished meal.</p>
     <h3>Perfection Every Time</h3>
     <p>With our help you will be able to prepare delicious food at home and achieve perfection every time. Satisfaction guaranteed.</p>
-    <a href="about.php" class="button">Learn More</a>
+    <a href="about.php" class="nav_link red-btn">Learn More</a>
   </section>
 
   <!-- Featured Recipes -->
   <secion id="featured">
     <h2>Featured Meals</h2>
-    <div class="meal">
-      <img src="img/recipes/beef_medallions_and_mushroom_sauce/0101_2PM_Steak-Diane_97315_SQ_hi_res.jpg" alt="Beef Medallions & Mushroom Sauce">
-      <h4>Beef Medallions & Mushroom Sauce</h4>
-      <h5 class="subtitle">with Mashed Potatoes</h5>
-      <p>In this easy recipe, inspired by steak Diane (an American...</p>
-      <a href="#">Get Cooking</a>
+    <div class="recipe">
+      <img class="rec_img" src="img/recipes/beef_medallions_and_mushroom_sauce/0101_2PM_Steak-Diane_97315_SQ_hi_res.jpg" alt="Beef Medallions & Mushroom Sauce">
+      <h4 class="rec_title">Beef Medallions & Mushroom Sauce</h4>
+      <h5 class="rec_subtitle">with Mashed Potatoes</h5>
+      <p class="rec_desc">In this easy recipe, inspired by steak Diane (an American...</p>
+      <a class="red-btn rec-btn" href="#">Get Cooking</a>
     </div>
-    <div class="meal">
-      <img src="img/recipes/brocolli_and_basil_pesto_sandwhiches/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
-      <h4>Broccoli & Basil Pesto Sandwiches</h4>
-      <h5 class="subtitle">with Romaine & Citrus Salad</h5>
-      <p>These Italian focaccia sandwiches are layered with broccoli, three cheeses...</p>
-      <a href="#">Get Cooking</a>
+    <div class="recipe">
+      <img class="rec_img" src="img/recipes/brocolli_and_basil_pesto_sandwhiches/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
+      <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
+      <h5 class="rec_subtitle">with Romaine & Citrus Salad</h5>
+      <p class="rec_desc">These Italian focaccia sandwiches are layered with...</p>
+      <a class="red-btn rec-btn" href="#">Get Cooking</a>
     </div>
   </secion>
 </main>
