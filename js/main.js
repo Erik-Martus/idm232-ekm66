926317b7d23295e921f7c2dec445c159d3dc71ac
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-16 00:48:34 -0400

    :tada: complete alpha styling for recipe template
    Completed alpha styling for recipe template page.

--- js/main.js
+++ js/main.js
@@ -21,3 +21,23 @@ toggle.addEventListener('mousedown', function () {
     filters.classList.add("hidden");
   }
 }, false);
+
+// ! Recipe Image Toggle
+// This does not work yet, it will be revisited later.
+var img_on = document.getElementById("toggle-on"); // "On" Radio button
+var img_off = document.getElementById("toggle-off"); // "Off" Radio button
+var rec_steps = document.getElementById("dir_list"); // step by step directions
+var step_img = document.getElementsByClassName("step_img");
+console.log("TCL: step_img", step_img)
+
+img_on.addEventListener('mousedown', function () {
+  if (step_img.classList.contains("hidden") = true) {
+    // step_img.classList.remove("hidden");
+    console.log('True')
+  }
+}, false);
+
+img_off.addEventListener('mousedown', function () {
+  step_img.classList.add("hidden");
+  console.log('show')
+}, false);
