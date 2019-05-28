5a55a6b9b1e712964997e099c9761e1d952358e4
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-28 17:49:19 -0400

    :arrow_up_small: Update to Featured Recipe section
    Updated featured recipes to grid layout

--- index.php
+++ index.php
@@ -3,14 +3,7 @@
 ?>
 
 <main>
-  <section id="hero">
-    <a id="hero_img" href="recipeTemplate.php">
-      <img src="img/recipes/01/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Ancho-Orange Chicken">
-      <h3>Ancho-Orange Chicken</h3>
-    </a>
-    <h5 class="rec_subtitle">with Kale, Rice & Roasted Carrots</h5>
-    <a class="red-btn rec-btn" href="recipeTemplate.php">Get Cooking</a>
-  </section>
+  <section id="hero" class="index_hero"></section>
 
   <!-- Mission Statement -->
   <section id="mission">
@@ -21,7 +14,7 @@
     <p>Cooking isn't difficult, everyone can do it (yes, even you). Our recipes come with step-by-step instructions that guide you from raw ingredients through finished meal.</p>
     <h3>Perfection Every Time</h3>
     <p>With our help you will be able to prepare delicious food at home and achieve perfection every time. Satisfaction guaranteed.</p>
-    <a href="about.php" class="nav_link red-btn">Learn More</a>
+    <a href="about.php" class="full_btn red-btn">Learn More</a>
   </section>
 
   <!-- Featured Recipes -->
@@ -30,13 +23,31 @@
     <div class="recipe">
       <img class="rec_img" src="img/recipes/02/0101_2PM_Steak-Diane_97315_SQ_hi_res.jpg" alt="Beef Medallions & Mushroom Sauce">
       <h4 class="rec_title">Beef Medallions & Mushroom Sauce</h4>
-      <h5 class="rec_subtitle">with Mashed Potatoes</h5>
       <a class="red-btn rec-btn" href="#">Get Cooking</a>
     </div>
     <div class="recipe">
       <img class="rec_img" src="img/recipes/03/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
       <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
-      <h5 class="rec_subtitle">with Romaine & Citrus Salad</h5>
+      <a class="red-btn rec-btn" href="#">Get Cooking</a>
+    </div>
+    <div class="recipe">
+      <img class="rec_img" src="img/recipes/03/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
+      <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
+      <a class="red-btn rec-btn" href="#">Get Cooking</a>
+    </div>
+    <div class="recipe">
+      <img class="rec_img" src="img/recipes/03/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
+      <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
+      <a class="red-btn rec-btn" href="#">Get Cooking</a>
+    </div>
+    <div class="recipe">
+      <img class="rec_img" src="img/recipes/03/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
+      <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
+      <a class="red-btn rec-btn" href="#">Get Cooking</a>
+    </div>
+    <div class="recipe">
+      <img class="rec_img" src="img/recipes/03/1225_FPV_Pesto_-Broccoli-Sandwich_74916_WEB_SQ_hi_res.jpg" alt="Brocolli and Basil Pesto Sandwhiches">
+      <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
       <a class="red-btn rec-btn" href="#">Get Cooking</a>
     </div>
   </secion>
