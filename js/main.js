e09f5347581f9772419d13101852c598ba1de366
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-30 03:02:16 -0400

    :tada: Completed alpa crit edits
    Finished edits recommended by Phil during the alpha crit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,14 @@
-// ! Menu Toggle
-var burger = document.getElementById("burger"); // Hamburger icon
-var menu = document.getElementById("menu"); // Drop down
-var viewport = window.innerWidth || document.documentElement.clientWidth;
+var filter_tog = document.getElementById("filter_toggle");
+var filter_opt = document.getElementById("filter_options");
 
-burger.addEventListener('mousedown', function () {
-  if (menu.classList.contains("menu_hidden")) {
-    menu.classList.remove("menu_hidden");
+function opt_tog() {
+
+}
+
+filter_tog.addEventListener("click", () => {
+  if (filter_opt.classList.contains("collapse")) {
+    filter_opt.classList.remove("collapse");
   } else {
-    menu.classList.add("menu_hidden");
+    filter_opt.classList.add("collapse");
   }
-}, false);
-
-window.addEventListener('resize', function () {
-  if (viewport > 1024) {
-    console.log('Desktop');
-  } else {
-    console.log('tablet');
-  }
-}, false);
-
-// ! Filter Toggle
-var toggle = document.getElementById("filter_trigger"); // Browse Recipes button
-var filters = document.getElementById("filter_drop"); // filter option container
-
-toggle.addEventListener('mousedown', function () {
-  if (filters.classList.contains("hidden")) {
-    filters.classList.remove("hidden");
-  } else {
-    filters.classList.add("hidden");
-  }
-}, false);
+}, false)
