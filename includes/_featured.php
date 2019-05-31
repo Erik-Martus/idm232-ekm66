428f1e03db15f95d6c82462e4f125f15aee02562
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-05-31 02:17:41 -0400

    :arrow_up_small: Add php functionality to _featured.php
    Began adding php functionality to _featured.php.
    
    Successfully got random number generator to work.
    
    Issues: only returning/echoing one instance

--- includes/_featured.php
+++ includes/_featured.php
@@ -1,5 +1,5 @@
 <!-- Featured Recipes -->
-  <section id="featured">
+  <!-- <section id="featured">
     <h2>Featured Meals</h2>
     <div class="recipe">
       <img class="rec_img" src="img/recipes/02/0101_2PM_Steak-Diane_97315_SQ_hi_res.jpg" alt="Beef Medallions & Mushroom Sauce">
@@ -31,4 +31,33 @@
       <h4 class="rec_title">Broccoli & Basil Pesto Sandwiches</h4>
       <a class="red-btn rec-btn" href="#">Get Cooking</a>
     </div>
-  </section>
+  </section> -->
+
+<?php
+  $random_id = randomNumber(0, 40, 6);
+  $random_id = join(', ', $random_id);
+  print_r($random_id);
+
+  $query_info = "SELECT id, title ";
+  $query_info .= "FROM rec_info ";
+  $query_info .= "WHERE id IN ('$random_id')";
+  // $query_info .= "WHERE id = 1";
+
+  $result_info = mysqli_query($connection, $query_info);
+
+  if (!$result_info) {
+    die("Database connection failed.");
+  }
+
+  $recipe_name = mysqli_fetch_assoc($result_info);
+  print_r($recipe_name);
+
+  foreach ($recipe_name as $recipe_name) {
+    echo "Recipe Name: {$recipe_name}<br>";
+  }
+
+  // while ($recipe = mysqli_fetch_assoc($result_info)) {
+  //   echo $recipe["title"];
+  //   $recipe++;
+  // }
+?>
