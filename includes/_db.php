6af5824736ab425a095a0c23c723eff5bfadb9e4
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-06-05 13:48:29 -0400

    :arrow_up_small: Add remote db connection
    Added successful connection to db on bluehost server.

--- includes/_db.php
+++ includes/_db.php
@@ -1,8 +1,20 @@
 <?php
+
+$host = $_SERVER['HTTP_HOST'];
+if ($host == 'localhost') {
+  // Local database credentials
   define('DB_SERVER', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
   define('DB_NAME', 'idm232-home_chef');
+}
+else {
+  // Remote database credentials
+  define('DB_SERVER', 'www.erikmartus.com');
+  define('DB_USER', 'erikmart_idm232');
+  define('DB_PASS', '1JZTL~77-Wk7');
+  define('DB_NAME', 'erikmart_idm232');
+}
 
   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
