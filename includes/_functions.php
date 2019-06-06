a56d58aadd03bb6b7c712dd57aae1e6af910038a
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-06-06 12:56:57 -0400

    :heavy_minus_sign: Remove safeURL function
    Removed safeURL function that was not working.

--- includes/_functions.php
+++ includes/_functions.php
@@ -11,16 +11,4 @@ function randomNumber($min, $max, $quantity) {
   shuffle($numbers);
   return array_slice($numbers, 0, $quantity);
 }
-
-function safeURL($page, $param) {
-  $url = rawurldecode($page);
-  $url .= "?" . $param . "=" ;
-  if (!is_numeric($param)) {
-    $url .= urldecode($param);
-  } else if (is_numeric($param)) {
-    $url .= $param;
-  }
-
-  return $url;
-}
 ?>
