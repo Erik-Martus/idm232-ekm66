25da53618a5909ae28db08187be06dddfcb5d821
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-06-09 01:05:47 -0400

    :arrow_up_small: Update browse to include all recipes
    Updated browse.php to include all recipes.
    
    TODO: optimize images

--- browse.php
+++ browse.php
@@ -1,8 +1,24 @@
 <?php require "includes/_head.php"; ?>
 
 <main>
-
+  <section id="all_rec" class="rec_grid">
+    <h2>Browse all Recipes</h2>
+    <?php
+      $query = "SELECT id, title, hero_image ";
+      $query .= "FROM recipes ";
+      $query .= "ORDER by id ASC";
+  
+      $result = mysqli_query($connection, $query);
+  
+      if (!$result) {
+        die("Database connection failed.");
+      }
+      
+      while($recipe = mysqli_fetch_assoc($result)) {
+        include "includes/_item.php";
+      } // End while loop
+    ?>
+  </section>
 </main>
 
 <?php require "includes/_footer.php"; ?>
-<?php mysqli_close($connection); ?>
