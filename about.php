b80903a8fbac277a8a7a784545e4ce1dabe65177
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-06-11 16:46:20 -0400

    :arrow_up_small: Update to about.php content
    Added more content to about.php

--- about.php
+++ about.php
@@ -1,7 +1,8 @@
 <?php 
   require_once "includes/_global.php";
   $title .= " | About Us";
-  require "includes/_head.php"; ?>
+  require "includes/_head.php";
+?>
 
 <main>
   <section id="hero" class="index_hero"></section>
@@ -18,12 +19,17 @@
   </section>
 
   <section id="about">
-    <h2>Who we are</h2>
-    <p>Founded in 2019 at Drexel University, Home Chef is </p>
+    <h2>Who We Are</h2>
+    <p>Founded in 2019 at Drexel University, Home Chef is a web based company dedicated to providing quality food for every customer. Our delicious recipes are guaranteed to please and are guaranteed healthy. By eating our food we promise you that all aspects of your life are bound to improve, all you have to do is follow our step by step instructions. Each recipe is set up that even those with no cooking experience can create the best food that they possibly can.</p>
+
+    <p>We also make sure that our recipes are perfect for portioning. We conveniently label all of recipes according to how many people you can serve. Now you can avoid the awkward moments when you don't prepare enough food for your guests. If it's date night, family night, a party, or the in-laws are over, rest assured that Home Chef has you covered every step of the way.</p>
+  </section>
+  <section id="action">
+    <h3>All That's left is for you to get out there and start cooking.</h3>
+    <a href="browse.php" class="red-btn rec-btn">See Our Recipes</a>
   </section>
 </main>
 
 <?php 
   require "includes/_footer.php"; 
-  mysqli_free_result($result);
 ?>
