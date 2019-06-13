b0fe8bce386f7f508d7679b046984d39e17abe53
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-06-13 18:46:57 -0400

    Everything is broken

--- admin/js/main.js
+++ admin/js/main.js
@@ -4,20 +4,22 @@
 (function (document, window, index) {
   var inputs = document.querySelectorAll('.inputfile');
   Array.prototype.forEach.call(inputs, function (input) {
-    var label = input.nextElementSibling,
-      labelVal = label.innerHTML;
+    var label = input.nextElementSibling;
+    var labelVal = label.innerHTML;
 
     input.addEventListener('change', function (e) {
       var fileName = '';
-      if (this.files && this.files.length > 1)
+      if (this.files && this.files.length > 1) {
         fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-      else
+      } else {
         fileName = e.target.value.split('\\').pop();
+      }
 
-      if (fileName)
+      if (fileName) {
         label.querySelector('span').innerHTML = fileName;
-      else
+      } else {
         label.innerHTML = labelVal;
+      }
     });
 
     // Firefox bug fix
--- admin/recipe.php
+++ admin/recipe.php
@@ -3,35 +3,97 @@
 require_once "../includes/_global.php";
 require_once "../includes/_db.php";
 
-require_once "includes/_head.php";
-
 if (isset($_GET["id"])) {
   $id = $_GET["id"];
 
   $safe_id = mysqli_real_escape_string($connection, $id);
 }
 
+if (isset($_POST["submit"])) {
 
+  $rec_title = mysqli_real_escape_string($connection, $_POST['rec-title']);
+  $rec_side = mysqli_real_escape_string($connection, $_POST['rec-side']);
+  if(isset($_POST['rec-hero'])) {
+    $rec_hero = $_POST['rec-hero'];
+    $rec_hero = $rec_hero[0];
+    $test = gettype($rec_hero);
+    $rec_hero = mysqli_real_escape_string($connection, $rec_hero);
+  }
+  print_r($_POST["rec-hero"]); 
+  // TODO: $rec_thumb_sm = mysqli_real_escape_string($connection, $_POST['rec-thumb_sm']);
+  // TODO: $rec_thumb_lg = mysqli_real_escape_string($connection, $_POST['rec-thumb_lg']);
+  // TODO: $rec_desc = mysqli_real_escape_string($connection, $_POST['rec-desc']);
+  $rec_time = mysqli_real_escape_string($connection, $_POST['rec-time']);
+  $rec_serv = mysqli_real_escape_string($connection, $_POST['rec-serv']);
+  $rec_nutr = mysqli_real_escape_string($connection, $_POST['rec-nutr']);
+  // TODO: $rec_ing = mysqli_real_escape_string($connection, $_POST['rec-ing']);
+  // TODO: $rec_ing_img = mysqli_real_escape_string($connection, $_POST['rec-ing_img']);
+  $rec_tool_title = mysqli_real_escape_string($connection, $_POST['rec-tool_title']);
+  $rec_tool_desc = mysqli_real_escape_string($connection, $_POST['rec-tool_desc']);
+  // TODO: $rec_tool_img = mysqli_real_escape_string($connection, $_POST['rec-tool_img']);
+  // TODO: $rec_step = mysqli_real_escape_string($connection, $_POST['rec-step']);
+  // TODO: $rec_step_img = mysqli_real_escape_string($connection, $_POST['rec-step_img']);
 
-if (isset($safe_id)) {
-  $query = 'SELECT * ';
-  $query .= 'FROM recipes ';
+  // Form validation
+
+  $query = 'UPDATE recipes SET ';
+  $query .= "title = '{$rec_title}', ";
+  $query .= "side = '{$rec_side}', ";
+  if (isset($rec_hero)) {
+    print_r($rec_hero);
+    $query .= "hero_image = '{$rec_hero}', ";
+  }
+  // TODO: $query .= "description = '{$rec_desc}', ";
+  $query .= "time = '{$rec_time}', ";
+  $query .= "servings = '{$rec_serv}', ";
+  $query .= "nutrition = '{$rec_nutr}' ";
+  // TODO: $query .= "ingredients = '{$rec_ing}', ";
+  // TODO: $query .= "ing_img = '{$rec_ing_img}', ";
+  // $query .= "kitchen_tool_desc = '{$rec_tool_desc}', ";
+  // TODO: $query .= "tool_img = '{$rec_tool_img}', ";
+  // TODO: $query .= "step_img_high = '{$rec_step_img}', ";
+  // TODO: $query .= "steps = '{$rec_step}' ";
+
   $query .= "WHERE id = {$safe_id} ";
   $query .= 'LIMIT 1';
 
-  $result = mysqli_query($connection, $query);
+  $result_update = mysqli_query($connection, $query);
 
-  if (!$result) {
-    die('Database query failed.');
+  if ($result_update && mysqli_affected_rows($connection) == 1) {
+    // success
+    $message = '<div class="alert alert-success" role="alert">Page Updated!</div>';
+  } else {
+    $message = '<div class="alert alert-danger" role="alert">Page update failed!</div>';
   }
+}
 
-
-  while ($recipe = mysqli_fetch_assoc($result)) {
-
+require_once "includes/_head.php";
 ?>
 
 <main>
-  <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>?id=<?php echo $safe_id; ?>" id="rec-form">
+<?php
+  if (isset($message)) {
+    echo $message;
+  }
+?>
+
+  <?php
+    if (isset($safe_id)) {
+      $query = 'SELECT * ';
+      $query .= 'FROM recipes ';
+      $query .= "WHERE id = {$safe_id} ";
+      $query .= 'LIMIT 1';
+
+      $result = mysqli_query($connection, $query);
+
+      if (!$result) {
+        die('Database query failed.');
+      }
+
+
+      while ($recipe = mysqli_fetch_assoc($result)) {
+  ?>
+  <form method="POST" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>?id=<?php echo $safe_id; ?>" id="rec-form">
     <h2>General Recipe Information</h2>
     <div class="input-cont">
       <label for="rec-title">Recipe Title</label>
@@ -46,7 +108,24 @@ if (isset($safe_id)) {
     <div class="input-cont">
       <label for="rec-hero">Main Image</label>
       <input type="file" name="rec-hero[]" id="rec-hero" class="form-item inputfile" accept="image/jpg, image/png">
-      <label for="rec-hero" class="file-label">Choose a file&hellip;</label>
+      <label for="rec-hero" class="file-label"><span></span>Choose a file&hellip;</label>
+      <?php
+        if (isset($_GET["rec-hero"])) {
+          echo "set";
+        }
+      ?>
+    </div>
+
+    <div class="input-cont">
+      <label for="rec-thumb_sm">Small Thumbnail</label>
+      <input type="file" name="rec-thumb_sm[]" id="rec-thumb_sm" class="form-item inputfile" accept="image/jpg, image/png">
+      <label for="rec-thumb_sm" class="file-label"><span></span>Choose a file&hellip;</label>
+    </div>
+    
+    <div class="input-cont">
+      <label for="rec-thumb_lg">Large Thumbnail</label>
+      <input type="file" name="rec-thumb_lg[]" id="rec-thumb_lg" class="form-item inputfile" accept="image/jpg, image/png">
+      <label for="rec-thumb_lg" class="file-label"><span></span>Choose a file&hellip;</label>
     </div>
     
     <div class="input-cont">
@@ -108,7 +187,7 @@ if (isset($safe_id)) {
     <div class="input-cont">
       <label for="rec-ing_img">Ingredients Image</label>
       <input type="file" name="rec-ing_img[]" id="rec-ing_img" class="form-item inputfile" accept="image/jpg, image/png">
-      <label for="rec-ing_img" class="file-label">Choose a file&hellip;</label>
+      <label for="rec-ing_img" class="file-label"><span></span>Choose a file&hellip;</label>
     </div>
 
     <h2>Kitchen Tool</h2>
@@ -131,7 +210,7 @@ if (isset($safe_id)) {
     <div class="input-cont">
       <label for="rec-tool_img">Kitchen Tool Image</label>
       <input type="file" name="rec-tool_img[]" id="rec-tool_img" class="form-item inputfile" accept="image/jpg, image/png" data-multiple-caption="{count} files selected" multiple>
-      <label for="rec-tool_img" class="file-label">Choose a file&hellip;</label>
+      <label for="rec-tool_img" class="file-label"><span></span>Choose a file&hellip;</label>
     </div>
 
     <h2>Step by Step Instructions</h2>
@@ -189,11 +268,11 @@ if (isset($safe_id)) {
     <div class="input-cont">
       <label for="rec-step_img">Step Images</label>
       <input type="file" name="rec-step_img[]" id="rec-step_img" class="form-item inputfile" accept="image/jpg, image/png" data-multiple-caption="{count} files selected" multiple>
-      <label for="rec-step_img" class="file-label">Choose a file&hellip;</label>
+      <label for="rec-step_img" class="file-label"><span></span>Choose a file&hellip;</label>
     </div>
 
     <div class="input-cont">
-      <input type="submit" value="Submit" class="nav-link">
+      <input type="submit" value="Submit" name="submit" class="nav-link" method="POST" enctype="multipart/form-data">
     </div>
   </form>
 </main>
